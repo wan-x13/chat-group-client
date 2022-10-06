b6506b4e4621be7c1404ef821e4651b9a48e6cc2
David Wanga <davidwangajean@gmail.com>
2022-10-06 15:52:13 +0100

    add routes from app and configure client socket

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home';
+import io from 'socket.io-client'
 import "./App.css"
 
+const socket =  io.connect('http://localhost:4000')
+
 const App = () => {
+
+    const [ username , setUsername] = useState('')
+    const [room , setRoom] = useState('')
     return (
        <BrowserRouter>
        <div className='App'>
        <Routes>
-            <Route path='/' element={<Home/>}/>
+            <Route path='/' element={<Home
+             username = {username}
+             setUsername = {setUsername}
+             room = {room}
+             setRoom = {setRoom}
+             socket = {socket}
+            />}/>
          </Routes>
 
        </div>
